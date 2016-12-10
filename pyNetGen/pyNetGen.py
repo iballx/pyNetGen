cf91905553ec2f737211998f16528d925425cc3d
Andrew Todd <andrew@todd-net.co.uk>
2016-12-10 17:54:23 +0000

    Update to cope with delimiters within comments Add option to report only singe pin nets Change output format

--- pyNetGen/pyNetGen.py
+++ pyNetGen/pyNetGen.py
@@ -1,13 +1,20 @@
+import sys
+import csv
+
 def main():
-    in_file = open("C:\\Users\\Andrew\\Documents\\netlist.csv",'r')
+    in_file = open(sys.argv[1],'r',newline='')
+    csvnetreader = csv.reader(in_file)
     ColList = []
     Netlist = {}
     StoreNets = False
-    for LineNo, LineText in enumerate(in_file):
-        cells = [x.strip() for x in LineText.split(',')]
-        if LineNo == 1:
+    SinglePinNets = False
+    if len(sys.argv) == 5 and sys.argv[4] == 's':
+        SinglePinNets = True
+    for LineNo, LineText in enumerate(csvnetreader):
+        cells = [x.strip() for x in LineText]
+        if LineNo == int(sys.argv[2]):
             for ColNo,CellText in enumerate(cells):
-                if (CellText.strip().lower() == "connector") & (ColNo > 0):
+                if (CellText.strip().lower() == "connector") & (ColNo > int(sys.argv[3])):
                     ColList.append(ColNo)
             StoreNets = True # Now that we have the connector columns we can start storing the nets
         elif StoreNets:
@@ -17,20 +24,21 @@ def main():
                         Netlist[cells[0]] = []
                     Netlist[cells[0]].append(cells[x]+':'+cells[x+1])
     in_file.close()
-    MaxNetNameLength = max(len(x) for x in Netlist.keys())
-    MaxLineLength = 20
+    MaxNetNameLength = max(len(x) for x in Netlist.keys())+1
+    MaxLineLength = 1000
     out_file = open("netlist.txt",'w')
     for x in sorted(Netlist.keys(),key=str.lower):
-        s = str.format("{0:.<{1}}".format(x,MaxNetNameLength))+' '
-        for i,v in enumerate(Netlist[x]):
-            if len(s) + len(v) + 1 > MaxLineLength:
-                out_file.write(s+'\n')
-                s = (MaxNetNameLength+1)*" "
-            s += v;
-            test = len(Netlist[x])
-            if i != len(Netlist[x])-1:
-                s += ','
-        out_file.write(s+'\n')
+        if (SinglePinNets and len(Netlist[x]) == 1) or not SinglePinNets:
+            s = str.format("{0: <{1}}".format(x+',',MaxNetNameLength))+' '
+            for i,v in enumerate(Netlist[x]):
+                if len(s) + len(v) + 1 > MaxLineLength:
+                    out_file.write(s+'\n')
+                    s = (MaxNetNameLength+1)*" "
+                s += v;
+                test = len(Netlist[x])
+                if i != len(Netlist[x])-1:
+                    s += ','
+            out_file.write(s+'\n')
     out_file.close()
 
 if __name__ == "__main__":
